6adfad53e619ae6acc98ec43f297ab70d43a26a1
RohanKondam <84554858+rohan-0926@users.noreply.github.com>
2022-05-27 21:12:48 +0530

    Create implementstrstr.cpp

---
+++ implementstrstr.cpp
@@ -0,0 +1,14 @@
+Day - 24 Implement strStr()
+Link - https://leetcode.com/problems/implement-strstr/
+
+Leetcode solution
+
+
+class Solution {
+public:
+    int strStr(string str1, string str2) {
+       int pos=str1.find(str2);
+        return pos;
+        
+    }
+};
